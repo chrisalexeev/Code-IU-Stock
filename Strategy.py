a6d431834c65400d4777ae88549a9556197c0a82
Zack Seliger <zseliger@gmail.com>
2020-07-09 15:23:51 -0500

    REALLY simple strategy support

--- Strategy.py
+++ Strategy.py
@@ -1,6 +1,51 @@
+from Study import *
+
+# super basic right now
 class Strategy:
-  def __init__(self):
+  def __init__(self, stock, balance=10000):
+    self.stock = stock
+    self.start_balance = balance
+    self.balance = balance
+    self.position_size = 0
+  
+  def buy(self, num, price):
+    self.balance -= num*price
+    self.position_size += num
+  
+  def sell(self, num, price):
+    self.balance += num*price
+    self.position_size -= num
+
+  def run(self):
+    for i in range(len(self.stock.data)):
+      self.manage(i)
+      
+  def manage(self, index):
+    # can buy or sell here
     pass
+
+  def getPL(self):
+    return (self.balance + self.position_size*self.stock.data["Close"][-1] - self.start_balance) / self.start_balance
+
+class BuyAndHold(Strategy):
+  def __init__(self, stock, balance=10000):
+    super().__init__(stock, balance)
+
+  def manage(self, index):
+    if index == 0:
+      self.buy(10, self.stock.data["Close"][index])
+
+class MovingAverageCrossover(Strategy):
+  def __init__(self, stock, balance=10000):
+    super().__init__(stock, balance)
+    self.shortMA = MovingAverage(period=12, type='exponential')(self.stock.data)['average']
+    self.longMA = MovingAverage(period=21, type='exponential')(self.stock.data)['average']
+  
+  def manage(self, index):
+    if self.shortMA[index] > self.longMA[index] and self.shortMA[index-1] <= self.longMA[index-1]:
+      self.sell(10, self.stock.data["Close"][index])
+    if self.longMA[index] < self.shortMA[index] and self.longMA[index-1] <= self.shortMA[index-1]:
+      self.buy(10, self.stock.data["Close"][index])
 
 '''
 How I (within reason) want a strategy to look like:
@@ -13,9 +58,10 @@ def __init__(self):
   self.diffValues = macd(baba)["diff"]
   self.longMa = average(baba)["average"]
 
-def buy(self, index):
-  return self.diffValues[index] > 0 and self.longMa.data[index] > baba.data["Close"][index]
+def manage(self, index):
+  if self.diffValues[index] > 0 and self.longMa.data[index] > baba.data["Close"][index]:
+    self.buy(10, self.baba.data["Close"][index])
 
-def sell(self, index):
-  return self.diffValues[index] < 0 and self.longMa.data[index] < baba.data["Close"][index]
+  if self.diffValues[index] < 0 and self.longMa.data[index] < baba.data["Close"][index]:
+    self.sell(10, self.baba.data["Close"][index])
 '''
--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 from Stock import Stock
 from Study import MovingAverage
 from Study import MACD
+from Strategy import BuyAndHold
+from Strategy import MovingAverageCrossover
 
 if __name__ == "__main__":
     baba = Stock("BABA", "BABA.csv")
 
     # add two moving average studies study
-    baba.add_study(MovingAverage(200, 'simple'))
-    baba.add_study(MovingAverage(50, 'simple'))
-    baba.add_study(MACD())
+    baba.add_study(MovingAverage(12, 'simple'))
+    baba.add_study(MovingAverage(21, 'simple'))
+    # baba.add_study(MACD())
+
+    # run strategies
+    buy_and_hold = BuyAndHold(baba)
+    ma_crossover = MovingAverageCrossover(baba)
+    buy_and_hold.run()
+    ma_crossover.run()
+
+    print(buy_and_hold.getPL())
+    print(ma_crossover.getPL())
 
     baba.plot()
